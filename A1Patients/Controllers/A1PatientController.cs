ec1d5286681048abfe933a8ed77e0bcf12cc1184
mary-far <68746110+mary-far@users.noreply.github.com>
2023-12-11 22:22:03 -0500

    Add files via upload

---
+++ A1Patients/Controllers/A1PatientController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using A1Patients.Models;
+
+namespace A1Patients.Controllers
+{
+    public class A1PatientController : Controller
+    {
+        private readonly PatientsContext _context;
+
+        public A1PatientController(PatientsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: A1Patient
+        public async Task<IActionResult> Index()
+        {
+            var patientsContext = _context.Patient.Include(p => p.ProvinceCodeNavigation);
+            return View(await patientsContext.ToListAsync());
+        }
+
+        // GET: A1Patient/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient
+                .Include(p => p.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.PatientId == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // GET: A1Patient/Create
+        public IActionResult Create()
+        {
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode");
+            return View();
+        }
+
+        // POST: A1Patient/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PatientId,FirstName,LastName,Address,City,ProvinceCode,PostalCode,Ohip,DateOfBirth,Deceased,DateOfDeath,HomePhone,Gender")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", patient.ProvinceCode);
+            return View(patient);
+        }
+
+        // GET: A1Patient/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", patient.ProvinceCode);
+            return View(patient);
+        }
+
+        // POST: A1Patient/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PatientId,FirstName,LastName,Address,City,ProvinceCode,PostalCode,Ohip,DateOfBirth,Deceased,DateOfDeath,HomePhone,Gender")] Patient patient)
+        {
+            if (id != patient.PatientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientExists(patient.PatientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", patient.ProvinceCode);
+            return View(patient);
+        }
+
+        // GET: A1Patient/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patient
+                .Include(p => p.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.PatientId == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        // POST: A1Patient/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var patient = await _context.Patient.FindAsync(id);
+            _context.Patient.Remove(patient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientExists(int id)
+        {
+            return _context.Patient.Any(e => e.PatientId == id);
+        }
+    }
+}
